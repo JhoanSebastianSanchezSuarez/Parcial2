a66886ac992932ae769dffe2c4092b9b6c6223bd
JSSS <103890793+JhoanSebastianSanchezSuarez@users.noreply.github.com>
2023-11-27 10:16:49 -0500

    Upcomming

--- src/app/conferencia/conferencia-list/conferencia-list.component.ts
+++ src/app/conferencia/conferencia-list/conferencia-list.component.ts
@@ -11,15 +11,29 @@ export class ConferenciaListComponent implements OnInit {
 
   conferencias: Array<Conferencia> = [];
 
+  proximas: number= 0;
+
+  currentDate: Date = new Date();
+
   constructor(private conferenciaService: ConferenciaService) { }
 
   getConferencias():void{
     this.conferenciaService.getConferencias().subscribe((conferencias) => {
-      this.conferencias = conferencias});
+      this.conferencias = conferencias;
+    });
+  }
+
+  getProximasConferencias():void{
+    this.conferencias.forEach(conferencia =>{
+      if(conferencia.starts > this.currentDate){
+        this.proximas += 1;
+      }
+    })
   }
 
   ngOnInit() {
     this.getConferencias();
+    this.getProximasConferencias();
   }
 
 }
