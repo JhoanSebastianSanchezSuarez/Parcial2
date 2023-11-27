bb482b90d32adc4b710b09a079a73fd0fb2120c5
JSSS <103890793+JhoanSebastianSanchezSuarez@users.noreply.github.com>
2023-11-27 10:05:09 -0500

    Listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConferenciaModule } from './conferencia/conferencia.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConferenciaModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conferencia/conferencia-list/conferencia-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ConferenciaService } from '../conferencia.service';
+import { Conferencia } from '../conferencia';
+
+@Component({
+  selector: 'app-conferencia-list',
+  templateUrl: './conferencia-list.component.html',
+  styleUrls: ['./conferencia-list.component.css']
+})
+export class ConferenciaListComponent implements OnInit {
+
+  conferencias: Array<Conferencia> = [];
+
+  constructor(private conferenciaService: ConferenciaService) { }
+
+  getConferencias():void{
+    this.conferenciaService.getConferencias().subscribe((conferencias) => {
+      this.conferencias = conferencias});
+  }
+
+  ngOnInit() {
+    this.getConferencias();
+  }
+
+}
